c66713153fc53453888a4b147f35fb2f42a0977b
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-16 01:08:45 +0300

    [Week-07] Running Sum of 1d Array - Ashraf

---
+++ Week-07/07-Running Sum of 1d Array/Ashraf.cpp
@@ -0,0 +1,13 @@
+// Author: Ashraf
+// https://leetcode.com/problems/running-sum-of-1d-array/description/
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
