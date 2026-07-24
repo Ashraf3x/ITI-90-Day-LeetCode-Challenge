6d05c837a3141a1a65b2c9557f4f0d78390fd7db
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-24 10:57:13 +0300

    [Week-08] Peak Index in a Mountain Array - Ashraf

---
+++ Week-08/05-Peak Index in a Mountain Array/Ashraf.cpp
@@ -0,0 +1,16 @@
+// Author: Ashraf
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+// Time Complexity: O(n log n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l=0, r=arr.size()-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(arr[m]<arr[m+1]) l=m+1;
+            else r=m;
+        }
+        return l;
+    }
+};
