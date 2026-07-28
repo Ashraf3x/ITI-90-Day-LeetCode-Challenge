244aadb36a2c4c86dfbf6346963fb51916c0f671
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-07-28 16:50:37 +0300

    [Week-09] Subsets - Ashraf

---
+++ Week-09/03-Subsets/Ashraf.cpp
@@ -0,0 +1,23 @@
+// Author: Ashraf
+// https://leetcode.com/problems/subsets/description/
+// Time Complexity: O(N * 2^N)
+// Space Complexity: O(N)
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        solve(0, nums, temp, res);
+        return res;
+    }
+    void solve(int i, vector<int>& nums, vector<int>& temp, vector<vector<int>>& res) {
+        if(i == nums.size()) {
+            res.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        solve(i + 1, nums, temp, res);
+        temp.pop_back();
+        solve(i + 1, nums, temp, res);
+    }
+};
