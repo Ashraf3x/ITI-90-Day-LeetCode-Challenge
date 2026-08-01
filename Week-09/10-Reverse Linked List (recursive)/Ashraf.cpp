1ba0c8f55e04574c9e41408dc68ce237d3e67d7b
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-08-01 10:28:44 +0300

    [Week-09] Reverse Linked List (recursive) - Ashraf

---
+++ Week-09/10-Reverse Linked List (recursive)/Ashraf.cpp
@@ -0,0 +1,26 @@
+// Author: Ashraf
+// https://leetcode.com/problems/reverse-linked-list/description/
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+};
